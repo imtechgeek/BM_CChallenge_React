715ba8d35d58762315a649011fe238bab4f9acc8
imtechgeek <usmanirfan996@gmail.com>
2022-09-07 23:07:47 +0200

    feat: add search component

---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,22 @@
+import { ChangeEvent, ChangeEventHandler } from "react";
+import styles from "./Search.module.css";
+
+interface Props {
+    handleChange: ChangeEventHandler;
+}
+export const Search = ({ handleChange }: Props) => {
+    
+    return (
+        <div className={styles.container}>
+            <div className={styles.searchBar}>
+                <input
+                    className={styles.input}
+                    onChange={handleChange}
+                    placeholder="Search Products"
+                    aria-label="search products"
+                    type="search"
+                />
+            </div>
+        </div>
+    );
+};
